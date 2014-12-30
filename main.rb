7d3cc99616eefb1f48d0d1cf6c8404f601d832bf
nicklegr <addr.for+github@gmail.com>
2014-12-30 19:42:15 +0900

    とりあえず動いた

---
+++ main.rb
@@ -0,0 +1,45 @@
+#!ruby -Ku
+# coding: utf-8
+
+require 'pp'
+require 'yaml'
+require 'twitter'
+require 'pry'
+require 'pry-nav'
+
+yaml = YAML.load_file('config.yaml')
+
+target = yaml['target']
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = yaml['consumer_key']
+  config.consumer_secret = yaml['consumer_secret']
+  config.access_token = yaml['oauth_token']
+  config.access_token_secret = yaml['oauth_token_secret']
+end
+
+def collect_with_max_id(collection=[], max_id=nil, &block)
+  response = yield(max_id)
+  collection += response
+  response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+end
+
+def client.get_all_tweets(user)
+  collect_with_max_id do |max_id|
+    options = {count: 200, include_rts: true, trim_user: true}
+    options[:max_id] = max_id unless max_id.nil?
+    user_timeline(user, options)
+  end
+end
+
+user = client.user(target).to_hash
+
+statuses = client.get_all_tweets(target)
+statuses.map! { |e| e.to_hash }
+
+data = {
+  'user' => user,
+  'statuses' => statuses,
+}
+
+puts data.to_yaml
